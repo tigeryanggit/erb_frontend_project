583e1705e4104b042102b7ffd9cacae5756b9f5b
tigeryanggit <tiger_yanghk@yahoo.com.hk>
2025-08-26 17:57:30 +0800

    Group Project 20250826

---
+++ react/src/components/Meat.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Cardmeat from './Cardmeat';
+import {cardmeat} from '../data';
+
+function Meat() {
+  return (
+    <div className='container-meat'>
+
+        <h2>肉類介紹</h2>
+        <br />
+        <p>說到買菜,當然小不了肉類.肉類在我們的飲中亦佔著重要的位置,這一節我們會為大家簡單介紹肉類的基本分類和智識。</p>
+
+        <h3>肉類的主要分類</h3>
+
+        {
+            cardmeat.map((card)=>(
+
+                <Cardmeat key={card.id} image={card.image} title={card.title} alt={card.alt} text={card.text} link={card.link} />
+            )
+
+            )
+
+        }
+        
+
+
+    </div>
+  )
+}
+
+export default Meat
---
+++ react/src/components/Redmeat.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Cardmeat from './Cardmeat';
+import CarouselBeef from './CarouselBeef';
+
+
+function Redmeat() {
+  return (
+    <>
+
+
+            <div class="card text-bg-light mb-3">
+                <div class="card-header"></div>
+                <div class="card-body">
+                    <h5 class="card-title">牛肉</h5>
+                    <p class="card-text">常見部位：</p> 
+                    <p class="card-text">牛腩、肋眼、西冷、牛柳、牛小排、牛腱等。</p>
+                    <p class="card-text">特點：</p> 
+                    <p class="card-text">風味濃郁，是全球消費最廣泛的紅肉之一。質地從軟嫩（如菲力）到富有嚼勁（如牛腱）不等。</p>
+                </div>
+            </div>
+
+    </>
+  )
+}
+
+export default Redmeat

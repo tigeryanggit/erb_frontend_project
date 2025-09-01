993af2db0e6c327242cea096b0847cf2960c720a
tigeryanggit <tiger_yanghk@yahoo.com.hk>
2025-09-01 23:05:39 +0800

    Project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/erb_frontend_project/",
+  // base: "/erb_frontend_project/",
   plugins: [react()],
 })
